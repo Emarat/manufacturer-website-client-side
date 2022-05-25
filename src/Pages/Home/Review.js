684d78a7de453c4e5146d48f79fa2c0f6c92a6a2
Emarat <emarathossain00@gmail.com>
2022-05-25 14:13:22 +0600

    load review from server

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Reviews from './Reviews';
 
 const Review = () => {
-    const reviews = [
-        {
-            _id: '1',
-            rating: '5',
-            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi.'
-        },
-        {
-            _id: '2',
-            rating: '4',
-            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi.'
-        },
-        {
-            _id: '3',
-            rating: '5',
-            comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi.'
-        },
-    ]
+
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/reviews')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    // const reviews = [
+    //     {
+    //         _id: '1',
+    //         "rating": "5",
+    //         "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi."
+    //     },
+    //     {
+    //         _id: '2',
+    //         "rating": "4",
+    //         "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi."
+    //     },
+    //     {
+    //         _id: '3',
+    //         "rating": "5",
+    //         "comment": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ornare, mi in ornare elementum, libero nibh lacinia urna, quis convallis lorem erat at purus. Maecenas eu varius nisi."
+    //     },
+    // ]
     return (
         <>
             <h2 className='mt-14 text-xl font-semibold divider'>CUSTOMER REVIEWS</h2>
