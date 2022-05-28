305bd48cf0c7dbadbfddf1f0e2581ff8019e7683
Emarat <emarathossain00@gmail.com>
2022-05-28 17:26:25 +0600

    make admin

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const UserRow = ({ user, index }) => {
-    const { email, name, address } = user;
+const UserRow = ({ user, refetch, index }) => {
+    const { email, role, name } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch();
+                console.log(data);
+                toast("Successfully Made an Admin");
+
+            })
+    }
+
     return (
         <tr>
             <th>{index + 1}</th>
             <td>{name}</td>
             <td>{email}</td>
-            <td>{address}</td>
+            <ToastContainer />
+            <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
+            <td><button class="btn btn-xs">Remove User</button></td>
         </tr>
     );
 };
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,7 @@ import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
@@ -18,7 +18,8 @@ const Users = () => {
                             <th></th>
                             <th>Name</th>
                             <th>Email</th>
-                            <th>Address</th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -27,6 +28,7 @@ const Users = () => {
                                 key={user._id}
                                 user={user}
                                 index={index}
+                                refetch={refetch}
                             ></UserRow>)
                         }
                     </tbody>
